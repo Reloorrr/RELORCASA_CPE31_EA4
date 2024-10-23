08dc7d3b1a63761737a57ab38d07f1db52de2da4
Rafael Luis F. Relorcasa <rafaelrelorcasa27@gmail.com>
2024-10-24 02:35:45 +0800

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,17 +10,91 @@ void main() {
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: const DicePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1, right = 1;
+  String resultMessage = '';
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: (){
+                    setState(() {
+                      ChangeValue();
+                    });
+                    print('Left image is clicked');
+                    print(left);
+                  },
+                  child: Image.asset('images/dice$left.png')),
+            )),
+              Expanded(child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                    onPressed: (){
+                      setState(() {
+                        ChangeValue();
+                      });
+                      print('Right image is clicked');
+                      print(right);
+                    },
+                    child: Image.asset('images/dice$right.png')),
+              )),
+            ],
+          ),
+          const SizedBox(height: 20), // Space between dice and text
+          Text(
+            'The total roll is ${left + right}',
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 10), // Space between the total roll and result message
+          Text(
+            resultMessage,
+            style: const TextStyle(
+              fontSize: 21,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void ChangeValue(){
+    left = 1 + Random().nextInt(6);
+    right = 1 + Random().nextInt(6);
+
+    if (left > right) {
+      resultMessage = 'Left dice rolls higher';
+    } else if (right > left) {
+      resultMessage = 'Right dice rolls higher';
+    } else {
+      resultMessage = 'Dice rolls are equal';
+    }
   }
 }
+
+
+
